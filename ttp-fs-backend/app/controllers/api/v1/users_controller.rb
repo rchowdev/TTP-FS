f781865d5b38be0da1359e6f2bea610617285034
rchow1944 <rchow2@buffalo.edu>
2019-06-22 18:19:57 -0400

    added sign up routes and action

--- ttp-fs-backend/app/controllers/api/v1/users_controller.rb
+++ ttp-fs-backend/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+  def create
+    #code
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: 'Failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  private
+  def user_params
+    #code
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
