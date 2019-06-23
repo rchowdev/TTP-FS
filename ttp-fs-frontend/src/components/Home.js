61b675f42aecb1daa0d120227b14a1138aa8019f
rchow1944 <rchow2@buffalo.edu>
2019-06-23 16:52:03 -0400

    added sign up form

--- ttp-fs-frontend/src/components/Home.js
+++ ttp-fs-frontend/src/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import SignUp from './auth/SignUp';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div>
+      <h1>Home</h1>
+      <SignUp />
+    </div>
   );
 }
 
