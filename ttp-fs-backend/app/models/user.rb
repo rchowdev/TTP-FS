a806648597e08b0f822bdb8447bfdd1dcf8eac28
rchow1944 <rchow2@buffalo.edu>
2019-06-25 19:45:08 -0400

    created has many through relationship between users and stocks

--- ttp-fs-backend/app/models/user.rb
+++ ttp-fs-backend/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
