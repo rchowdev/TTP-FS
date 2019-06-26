8a464044472dc80f3ab831b028a5d8f38c4418fd
rchow1944 <rchow2@buffalo.edu>
2019-06-26 01:56:36 -0400

    Added loading state. added fetch request to api to check if user has enough money

--- ttp-fs-frontend/src/components/user/portfolio/BuyMenu.js
+++ ttp-fs-frontend/src/components/user/portfolio/BuyMenu.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Form, Grid, Button } from 'semantic-ui-react';
 
 const BuyMenu = ({ balance }) => {
-  const handleSubmit = () => {
-    return;
+  const [ticker, setTicker] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const [loading, setLoading] = useState(true);
+
+  //Sets loading to true if user has enough money to buy
+  const enoughMoney = () => {
+    const formattedTicker = encodeURIComponent(ticker);
+    axios.get(`https://api.iextrading.com/1.0/tops?symbols=${formattedTicker}`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(false); //Set loading to false until api request resolves
+    enoughMoney();
   }
 
   return (
     <Grid.Column>
-      <Form size="large">
+      <Form size="large" loading={!loading} onSubmit={handleSubmit}>
         <Form.Field>
           <label>Ticker</label>
-          <input type="text" placeholder="Ticker" />
+          <input
+            type="text"
+            placeholder="Ticker"
+            value={ticker}
+            onChange={e => setTicker(e.target.value)}
+          />
         </Form.Field>
         <Form.Field>
           <label>Quantity</label>
-          <input type="number" placeholder="Qty" />
+          <input
+            type="number"
+            placeholder="Qty"
+            min="1"
+            value={quantity}
+            onChange={e => setQuantity(e.target.value)}
+          />
         </Form.Field>
         <Button type="submit" positive size="large" fluid>Buy</Button>
       </Form>
