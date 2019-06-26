721a8d48e0f4fea60ecda652a2dccbc6c6b76b5b
rchow1944 <rchow2@buffalo.edu>
2019-06-26 19:23:12 -0400

    refactored balance and ticker verification. Added error handling for invalid ticker

--- ttp-fs-frontend/src/components/user/portfolio/BuyMenu.js
+++ ttp-fs-frontend/src/components/user/portfolio/BuyMenu.js
@@ -9,20 +9,14 @@ const BuyMenu = ({ user, setUser, updateStocks }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   //Gets stock data from IEX API
   function getStockData() {
     const formattedTicker = encodeURIComponent(ticker); //API requires symbols to be URI encoded
     return axios.get(`https://api.iextrading.com/1.0/tops?symbols=${formattedTicker}`)
-      .then(res => res.data[0])
+      .then(res => res.data)
       .catch(err => console.log(err));
-  };
-
-  //If we have enough money return new balance else return negative number
-  function newBalance(stockPrice) {
-    const total = currency(stockPrice) * quantity;
-    const userBalance = currency(user.formatted_balance);
-    return userBalance > total ? userBalance.subtract(total).value : -1;
   };
 
   //Fetch request to API to patch user's balance and find or create stock
@@ -32,16 +26,45 @@ const BuyMenu = ({ user, setUser, updateStocks }) => {
       .catch(err => console.log(err));
   };
 
+  //Calculate new balance: Will be negative if not enough money
+  function newBalance(stockPrice) {
+    const total = currency(stockPrice) * quantity;
+    const userBalance = currency(user.formatted_balance);
+    return userBalance.subtract(total).value;
+  };
+
+  //Check if we have enough money to buy stocks
+  function verifyBalance(stockPrice) {
+    const balance = newBalance(stockPrice);
+    if (balance < 0) {
+      setErrorMessage("Not Enough Funds")
+      return false;
+    }
+    return true;
+  };
+
+  //Verify if we got a response from IEX API (Check if ticker was valid)
+  function verifyTicker(stocks) {
+    if(!stocks.length){
+      setErrorMessage("Invalid Ticker");
+      return false;
+    }
+    return true;
+  };
+
   /*Check if we have enough money. If we have enough money, then we make a patch request to user.
     Patch request will have stock, quantity bought, new balance
   */
   const handleSubmit = (e) => {
     e.preventDefault();
+    setSuccess(false);  //Reset error and success state
+    setError(false);
     setIsLoading(true); //Set loading to true until api request resolves
     getStockData()
-    .then(({ symbol, lastSalePrice }) => {
-      const balance = newBalance(lastSalePrice);
-      if(balance >= 0){ //Had enough money
+    .then(stocks => {
+      if(verifyTicker(stocks) && verifyBalance(stocks[0].lastSalePrice)){ //Check if ticker is valid, then if we have enough funds
+        const { symbol, lastSalePrice } = stocks[0];
+        const balance = newBalance(lastSalePrice);
         const orderData = {
           orderData: {
             symbol,
@@ -59,7 +82,7 @@ const BuyMenu = ({ user, setUser, updateStocks }) => {
             updateStocks({ symbol, quantity });
           })
           .catch(err => console.log(err));
-      } else {  //Didn't have enough money
+      } else {
         setError(true);
         setIsLoading(false);
       }
@@ -75,7 +98,7 @@ const BuyMenu = ({ user, setUser, updateStocks }) => {
         </Message>
         <Message error>
           <Message.Header>We're sorry. The transaction was unsuccessful</Message.Header>
-          <p>You do not have enough money in your balance!</p>
+          <p>{errorMessage}</p>
         </Message>
         <Form.Field>
           <label>Ticker</label>
