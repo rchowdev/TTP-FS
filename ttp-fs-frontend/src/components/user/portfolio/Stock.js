d6e2a9363972e4b32c043c84d4d0213672dce948
rchow1944 <rchow2@buffalo.edu>
2019-06-27 16:07:11 -0400

    added color state for portfolio

--- ttp-fs-frontend/src/components/user/portfolio/Stock.js
+++ ttp-fs-frontend/src/components/user/portfolio/Stock.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import currency from 'currency.js';
 import { List } from 'semantic-ui-react';
 
-const Stock = ({ stock: { symbol, latestPrice, quantity } }) => {
+const Stock = ({ stock: { symbol, latestPrice, quantity, open } }) => {
+  const [color, setColor] = useState("grey");
+  useEffect(() => {
+    if(latestPrice < open){
+      setColor("red");
+    } else if(latestPrice > open){
+      setColor("green");
+    } else {
+      setColor("grey");
+    }
+  }, [setColor, open, latestPrice]);
+
   return (
-    <List.Item>
+    <List.Item style={{ color }}>
       <List.Content floated="right">
         {currency(currency(latestPrice) * quantity, { formatWithSymbol: true }).format()}
       </List.Content>
