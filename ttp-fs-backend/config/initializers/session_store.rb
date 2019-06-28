315bd2efadd2f942f182ec0c1bbc314e9c927028
rchow1944 <rchow2@buffalo.edu>
2019-06-27 22:09:41 -0400

    added secure, expiration date, http only flag to session store

--- ttp-fs-backend/config/initializers/session_store.rb
+++ ttp-fs-backend/config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_ttp_fs_app"
+Rails.application.config.session_store :cookie_store,
+  :key => "_ttp_fs_app",
+  :expire_after => 4.hours,
+  :http_only => true,
+  :secure => true
